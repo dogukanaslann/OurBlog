1122cbb0c58497b2bd0f8a9cf97534a676f79070
dogukanaslann <mustafadogukanaslan@hotmail.com>
2023-09-20 23:52:40 +0300

    Add project files.

---
+++ OurBlog/Data/Post.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace OurBlog.Data
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [MaxLength(400)]
+        public string Title { get; set; } = null!;
+
+        public string? Content { get; set; }
+
+        public string? Image { get; set; }
+
+        public string AuthorId { get; set; } = null!;
+
+        public IdentityUser Author { get; set; } = null!;
+    }
+}
